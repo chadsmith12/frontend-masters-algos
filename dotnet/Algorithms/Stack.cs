edb9ab78f9856d76d0ed388a817e05d61615f236
Chad Smith <8794805+chadsmith12@users.noreply.github.com>
2023-04-08 15:14:37 -0500

    added stack data structure and tests for stack

---
+++ dotnet/Algorithms/Stack.cs
@@ -0,0 +1,61 @@
+﻿namespace Algorithms
+{
+    public class Stack<T>
+    {
+        private class Node
+        {
+            public T Value;
+            public Node? Previous;
+
+            public Node(T value) { Value = value; }
+        }
+
+        private int length = 0;
+        private Node? head = null;
+        private Node? tail = null;
+
+        public void Push(T value) 
+        {
+            var node = new Node(value);
+            length++;
+
+            if (this.tail == null)
+            {
+                this.head = node;
+                this.tail = node;
+                return;
+            }
+
+            var tail = this.tail;
+            this.tail = node;
+            this.tail.Previous = tail;
+        }
+
+        public T Pop() 
+        {
+            if (this.tail == null)
+            {
+                throw new InvalidOperationException("Can not pop when stack is empty");
+            }
+
+            length--;
+
+            var tail = this.tail;
+            this.tail = tail.Previous;
+
+            return tail.Value;
+        }
+
+        public T Peek() 
+        {
+            if (head == null)
+            {
+                throw new InvalidOperationException("Can not peek wihen stack is empty");
+            }
+
+            return head.Value;
+        }
+
+        public int Length { get { return length; } }
+    }
+}
---
+++ dotnet/AlgorithmsTests/StackTests.cs
@@ -0,0 +1,78 @@
+﻿using Shouldly;
+
+namespace AlgorithmsTests
+{
+    public class StackTests
+    {
+        [Fact]
+        public void PushFirstItemWilHaveLengthOfOne()
+        {
+            var queue = new Algorithms.Stack<int>();
+            queue.Push(1);
+
+            queue.Length.ShouldBe(1);
+        }
+
+        [Fact]
+        public void PushTwoTimesWillhaveLenthOfTwo()
+        {
+            var stack = new Algorithms.Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+
+            stack.Length.ShouldBe(2);
+        }
+
+        [Fact]
+        public void PeekWillReturnValue()
+        {
+            var stack = new Algorithms.Stack<int>();
+            stack.Push(1);
+
+            stack.Peek().ShouldBe(1);
+        }
+
+        [Fact]
+        public void PeekOnEmptyQueueWillThrow()
+        {
+            var stack = new Algorithms.Stack<int>();
+            Should.Throw(() => stack.Peek(), typeof(InvalidOperationException));
+        }
+
+        [Fact]
+        public void PoppingWillReturnFirstValue()
+        {
+            var stack = new Algorithms.Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(32);
+
+            stack.Pop().ShouldBe(32);
+        }
+
+        [Fact]
+        public void PoppingWillChangeLength()
+        {
+            var stack = new Algorithms.Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+
+            stack.Pop();
+
+            stack.Length.ShouldBe(1);
+        }
+
+        [Fact]
+        public void PoppingWillRemoveValue()
+        {
+            var stack = new Algorithms.Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+
+            var value = stack.Pop();
+
+            value.ShouldBe(2);
+            stack.Peek().ShouldBe(1);
+        }
+    }
+}
