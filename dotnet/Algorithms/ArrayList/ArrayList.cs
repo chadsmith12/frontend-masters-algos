810798ce3618718e5539399207e0bf555e5da667
Chad Smith <8794805+chadsmith12@users.noreply.github.com>
2023-04-08 22:14:29 -0500

    add very basic array list implementation with array backing that can resize

---
+++ dotnet/Algorithms/ArrayList/ArrayList.cs
@@ -0,0 +1,52 @@
+﻿namespace Algorithms.ArrayList
+{
+    public class ArrayList<T>
+    {
+        private int capacity;
+        private int length;
+        private T[] buffer;
+
+        public ArrayList(int capacity)
+        {
+            this.capacity = capacity;
+            this.length = 0;
+            this.buffer = new T[capacity];
+        }
+
+        public void Push(T item)
+        {
+            // do we need more memory?
+            if (this.length == capacity)
+            {
+                Reallocate();
+            }
+
+            buffer[this.length] = item;
+            this.length++;
+        }
+
+        public T Pop()
+        {
+            if (length == 0)
+            {
+                throw new InvalidOperationException("Can not pop from an empty ArrayList");
+            }
+
+            var value = buffer[this.length - 1];
+            this.length--;
+
+            return value;
+        }
+
+        private void Reallocate()
+        {
+            this.capacity *= 2;
+            var newBuffer = new T[this.capacity];
+            Array.Copy(buffer, newBuffer, buffer.Length);
+            this.buffer = newBuffer;
+        }
+
+        public int Capacity { get { return this.capacity; } }
+        public int Length { get { return this.length; } }
+    }
+}
---
+++ dotnet/AlgorithmsTests/ArrayListTests.cs
@@ -0,0 +1,48 @@
+﻿using Algorithms.ArrayList;
+using Shouldly;
+
+namespace AlgorithmsTests
+{
+    public class ArrayListTests
+    {
+        [Fact]
+        public void ArrayListHasInitialCapacity()
+        {
+            var list = new ArrayList<int>(4);
+
+            list.Capacity.ShouldBe(4);
+        }
+
+        [Fact]
+        public void CanPushItemInArrayList()
+        {
+            var list = new ArrayList<int>(4);
+            list.Push(2);
+
+            list.Length.ShouldBe(1);
+        }
+
+        [Fact]
+        public void CanPopItemFromArrayList()
+        {
+            var list = new ArrayList<int>(4);
+            list.Push(2);
+            list.Push(4);
+
+            var value = list.Pop();
+
+            value.ShouldBe(4);
+        }
+
+        [Fact]
+        public void ArrayListCanResize()
+        {
+            var list = new ArrayList<int>(2);
+            list.Push(1);
+            list.Push(2);
+            list.Push(3);
+
+            list.Capacity.ShouldBe(4);
+        }
+    }
+}
